accf221388967e585f3f99bafc309afb8cdfd35f
Laura Raynes <37169404+lraynes@users.noreply.github.com>
2018-06-13 21:47:26 -0700

    Delete gitignore.py

--- NewsMood/gitignore.py
+++
@@ -1,5 +0,0 @@
-# Twitter API Keys
-consumer_key = "bGkRdvfNcoYNcVUWXBSdZ1lSp"
-consumer_secret = "y1WYh5XMnk2WuhveydQy1HkOtQUAFtuEJZPNyJiY0XPPSGDOHi"
-access_token = "123408507-B7xX7Q1v74bClrDSo3wftwF29vtrA2LwlD2LQucx"
-access_token_secret = "6g9Kwmb0zWG7AOpQMxMIMW0I9CCvEsiZ5vQFbGWVvQnEK"
